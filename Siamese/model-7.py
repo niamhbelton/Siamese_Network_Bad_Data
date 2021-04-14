c94c797800850c0454dc7403e1bb43273da8a336
niamhbelton <71726124+niamhbelton@users.noreply.github.com>
2021-04-14 18:49:46 +0100

    Add files via upload

---
+++ Siamese/model-7.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.pretrained_model = models.alexnet(pretrained=True)
+        self.pooling_layer = nn.AdaptiveAvgPool2d(1)
+        self.fcOut = nn.Linear(256, 1)
+        self.sigmoid = nn.Sigmoid()
+        
+        
+    def forward(self, x1):
+        x1 = torch.squeeze(x1, dim=0)
+        features = self.pretrained_model.features(x1)
+        pooled_features = self.pooling_layer(features)
+        pooled_features = pooled_features.view(pooled_features.size(0), -1)
+        flattened_features1 = torch.max(pooled_features, 0, keepdim=True)[0]
+        x1 = self.sigmoid(flattened_features1)
+        return x1
+    
+
+        
+    
+    
+        
+        
+        
+        
+        
+        
+        
+    
